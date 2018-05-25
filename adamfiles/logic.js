3a06ce3ee828e7e0bb59a97d0ef90fe912b7cc1f
Adam Mercier <adammercier@Adams-MacBook-Pro.local>
2018-05-24 18:39:05 -0700

    updated popup fields

--- adamfiles/logic.js
+++ adamfiles/logic.js
@@ -1,28 +1,37 @@
 // Set variable array
 schooldata = [];
 
+// Set path to data
 var schoolsurl = "https://raw.githubusercontent.com/mayushah1/Best-Place-to-live/adam_branch/adamfiles/city_school.csv"
 
+// Read in csv and grab data
 d3.csv(schoolsurl, function(error, schooldata) {
   
   if (error) {console.log(error); return (error);
   schooldata.forEach(function(d) {
     d.lon = +d.lon;
     d.lat = +d.lat;
-    // d.city[0] = +d.city[0];
     d["city"] = d["city"];
     d.enrollment = +d.enrollment;
     d.parentrating = +d.parentrating;
   });
 };
-console.log(schooldata[0]);
 
+//Check what is being pulled
+// console.log(schooldata[0]);
+
+
+// Add map markers with popup box information
 var schoolMarkers = [];
 
 for (var i = 0; i < schooldata.length; i++) {
   // loop through the cities array, create a new marker, push it to the cityMarkers array
   schoolMarkers.push(
-    L.marker([schooldata[i].lat,schooldata[i].lon]).bindPopup("<h1>" + schooldata[i].city + "</h1>")
+    L.marker([schooldata[i].lat,schooldata[i].lon]).bindPopup("<h2>" 
+    + "City: " + schooldata[i].city + "<br>" 
+    + "Average School Size: " + Math.round(schooldata[i].enrollment,2).toFixed(1) + "<br>" 
+    + "Average Parent Rating: " + Math.round(schooldata[i].parentrating,2).toFixed(1) 
+    + "</h2>")
   );
 }
 
@@ -54,7 +63,7 @@ var overlayMaps = {
 // Create map object and set default layers
 var myMap = L.map("map", {
   center: [37.85, -122.24],
-  zoom: 6,
+  zoom: 6.2,
   layers: [light, cityLayer]
 });
 
