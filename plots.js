bf80e029a5af99e2059c21e4ae7557f09869c9a4
heidi_laptop_home <hmlanden@gmail.com>
2018-05-24 20:17:56 -0700

    add total enrollment plot

--- plots.js
+++ plots.js
@@ -1,6 +1,6 @@
     /*--------------------------------------------------------------------------
-        +++ Create public vs private school graph
-        --------------------------------------------------------------------------*/
+                                                                                                                +++ Create public vs private school graph
+                                                                                                                --------------------------------------------------------------------------*/
 
     var xBar = [];
     perPrivPub.forEach(function (data) {
@@ -16,14 +16,27 @@
     perPrivPub.forEach(function (data) {
         yBarTwo.push(data["Percent Private School"]);
     });
-    console.log(yBarTwo);
 
+    var xText = [];
+    perPrivPub.forEach(function (data) {
+        let num = Math.round(data['Total Enrolled'] * (data['Percent Public School'] / 100));
+        let result = num.toString() + ' Students'
+        xText.push(result);
+    });
+
+    var yText = [];
+    perPrivPub.forEach(function (data) {
+        let num = Math.round(data['Total Enrolled'] * (data['Percent Private School'] / 100));
+        let result = num.toString() + ' Students'
+        yText.push(result);
+    });
 
     //Set up bars
     var trace1 = {
         x: xBar,
         y: yBar,
-        name: '% Public School',
+        name: '% Public',
+        text: xText,
         type: 'bar',
         marker: {
             color: 'rgb(31, 72, 100)',
@@ -34,7 +47,8 @@
     var trace2 = {
         x: xBar,
         y: yBarTwo,
-        name: '% Private School',
+        name: '% Private',
+        text: yText,
         type: 'bar',
         marker: {
             color: 'rgb(233, 92, 64)',
@@ -47,6 +61,10 @@
     var layout = {
         title: 'Percent Public or Private School by School District',
         showlegend: true,
+        legend: {
+            x: 0,
+            y: 1.2,
+        },
         xaxis: {
             tickangle: -30
         },
@@ -55,3 +73,33 @@
 
     //plot shit
     Plotly.newPlot('plotPerPrivPub', allData, layout);
+
+
+
+    /*--------------------------------------------------------------------------
+        +++ Create total enrollment graph
+        --------------------------------------------------------------------------*/
+    var yBarThree = [];
+    perPrivPub.forEach(function (data) {
+        yBarThree.push(data["Total Enrolled"]);
+    });
+
+    var data2 = {
+        x: xBar,
+        y: yBarThree,
+        type: 'bar',
+        marker: {
+            color: 'rgb(255, 203, 53)',
+            opacity: 0.75
+        }
+    };
+
+    var layout2 = {
+        title: 'Total Students Enrolled by School District',
+        xaxis: {
+            tickangle: -30
+        }
+    };
+
+    //plot shit
+    Plotly.newPlot('totalEnrolled', [data2], layout2);
